ece18ab1d7986b54a303b997e26bc2966a9a6480
HaMaD <hamad.laaboun@gmail.com>
2023-04-07 10:59:37 +0300

    dataTypes , variables and output commands

---
+++ index.php
@@ -0,0 +1,48 @@
+<h1>Hello</h1>
+<?php
+/* -------- Output & Comments ------- */
+
+//! outputs 
+
+echo 123,' hello ',10.3;
+echo "</br>";  //just new line
+print 123;
+echo "</br>";  //just new line
+print_r([1,3,4]);
+echo "</br>";  //just new line
+var_dump('Hello');
+echo "</br>";  //just new line
+var_export('Hello');
+//*
+
+echo "</br>";
+//! Data types & variables
+
+$name = 'Hamad';
+$age = 21;
+$has_kids=false;
+echo $age;
+echo "</br>";
+echo $name;
+echo "</br>";
+echo $name . ' age '. $age;
+echo "</br>";
+echo "$name age $age";
+//?
+echo "</br>";
+echo '5+5= '.'5'+'5';
+define('gender','male');//const variable
+echo "</br>";
+echo gender //without $
+
+//*
+
+//! arrays
+
+
+
+
+
+
+?>
+
