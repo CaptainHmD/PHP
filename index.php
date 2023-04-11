16848a19b85dcc2f66d922c60e205576cd1058ad
HaMaD <hamad.laaboun@gmail.com>
2023-04-11 08:43:12 +0300

    array functions, string functions and superglobals

--- index.php
+++ index.php
@@ -43,6 +43,7 @@ echo "<br>";
 //simple array
 $array = [1, 2, 3, 4];
 $array2 = array(1, 2, 3, 4);
+$array.
 print_r($array2);
 
 echo "<br>";
@@ -124,8 +125,82 @@ test();
 //arrow function
 $arrowSum = fn($num1,$num2) => $num1+$num2;
 
+//! Array Functions
 
 
+echo "<br> Array functions <br> ";
+
+$names =["hamad","ibrahim","ahmed"];
+
+// get length
+echo 'length of names: '. count($names);
+echo '<br>';
+//! search by value and get boolean value
+$res=in_array('ahmed',$names);
+echo $res . '<br>';
+
+//! add to array at the end
+$names[]="name added"; 
+print_r($names);
+// array_push($array,"hi0","hi"); //you can add infinite at the end
+// array_unshift($names,"1","2","3");//you can add infinite at the beginning 
+echo '<br>';
+//! remove from arrays
+
+$values=array_pop($names); // remove from the end
+$values=array_shift($names);// remove at the begins
+unset($names[1]);// unset() destroys the specified variables.
+print_r($names);
+echo '<br>';
+
+//! merge arrays 
+$mergedArray=  array_merge($array,$array2);
+$mergedArray= [...$array,...$array2];
+
+//!array_combine
+$keys = [1,2,3,4];
+$colors=["green","blue","yellow","red"];
+$tempArray = array_combine($keys,$colors);// take values in array one as a key for values in array2
+echo '<br>';
+print_r($tempArray);
+echo '<br>';
+
+$flipped= array_flip($tempArray);// swap arrays and values
+print_r($flipped);
+echo '<br>';
+
+//! fill array with numbers
+
+$arrayOfNumbers = range(4,7); //The range() function creates an array containing a range of elements
+print_r($arrayOfNumbers);
+echo '<br>';
+
+//! map
+$newNumbersPlusOne = array_map(function($number){ // array map like js, the first parameter is the function with any name for the values, and the second is the array name
+  return "<br>" . $number. ' + 1: '.$number+1;
+},$arrayOfNumbers);
+
+//arrow fn
+$newNumbersPlusOne = array_map(fn($number)=>"<br> {$number} + 1 = ".$number+1,$arrayOfNumbers);
+
+print_r($newNumbersPlusOne);
+echo '<br>';
+
+//!filter
+$lessThan6 = array_filter($arrayOfNumbers,function($value){
+  return $value < 6;
+});
+
+//arrow fn
+$lessThan6 = array_filter($arrayOfNumbers,fn($value)=> $value < 6);
+
+print_r($lessThan6);
+echo "<br>";
+//! reduce 
+
+$sum = array_reduce($lessThan6,fn($previousValue,$currentValue)=>$previousValue+$currentValue);
+
+echo $sum . '<br>';
 
 
 
